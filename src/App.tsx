f6acc617e5f4e8271d0479e2ab88dcdbe6d0b3b0
Merey <crowtwentyohtwo@gmail.com>
2024-11-05 15:41:34 +0500

    input added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import Button from './components/Button/Button'
+import { MouseEvent } from "react";
+import Input from './components/Input/Input';
 
 function App() {
 
+  const addCounter = (e: MouseEvent) => {
+    console.log(e);
+  };
+
   return (
     <>
-      <Button >Button</Button>
+      <Button appearance='small' onClick={addCounter} >Button</Button>
+      <Button appearance='big' onClick={addCounter} >Button</Button>
+      <Input placeholder='Email'/>
     </>
-  )
-}
+  );
+};
 
 export default App
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,9 +2,12 @@ import { ButtonProps } from "./Button.props";
 import cn from 'classnames';
 import styles from './Button.module.css';
 
-const Button = ({ children, className, ...props }: ButtonProps) => {
+const Button = ({ children, className, appearance="small", ...props }: ButtonProps) => {
     return (
-        <button className={cn(styles['button'], styles['accent'], className)} {...props}>{children}</button>
+        <button className={cn(styles['button'], styles['accent'], className, {
+            [styles['small']]: appearance === "small",
+            [styles['big']]: appearance === "big"
+        })} {...props}>{children}</button>
     );
 };
 
